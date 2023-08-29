6a5755241ddde8355ac498c5dae7fa1cc3ab3337
Ishaan Asri <141492693+ia980@users.noreply.github.com>
2023-08-29 20:38:45 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,47 +1,24 @@
-def add(x, y):
-    return x + y
-
-def subtract(x, y):
-    return x - y
-
-def multiply(x, y):
-    return x * y
-
-def divide(x, y):
-    return x / y
-
-
 print("Select operation.")
 print("1.Add")
 print("2.Subtract")
 print("3.Multiply")
 print("4.Divide")
 
-while True:
-    choice = input("Enter choice(1/2/3/4): ")
+choice = input("Enter choice: ")
+Num1 = eval(input("Enter number 1:"))
+Num2 = eval(input("Enter number 2:"))
+if choice == "1":
+    result = Num1+Num2
+    print(Num1, "+", Num2, "=", result)
+elif choice == "2":
+    result = Num1 - Num2
+    print(Num1, "-", Num2, "=", result)
+elif choice == "3":
+    result = Num1 * Num2
+    print(Num1, "X", Num2, "=", result)
+elif choice == "4":
+    result = Num1 / Num2
+    print(Num1, "/", Num2, "=", result)
+else:
+    print("INVALID ENTRY!!")
 
-    if choice in ('1', '2', '3', '4'):
-        try:
-            num1 = float(input("Enter first number: "))
-            num2 = float(input("Enter second number: "))
-        except ValueError:
-            print("Invalid input, Please enter valid number")
-            continue
-
-        if choice == '1':
-            print(num1, "+", num2, "=", add(num1, num2))
-
-        elif choice == '2':
-            print(num1, "-", num2, "=", subtract(num1, num2))
-
-        elif choice == '3':
-            print(num1, "*", num2, "=", multiply(num1, num2))
-
-        elif choice == '4':
-            print(num1, "/", num2, "=", divide(num1, num2))
-            
-        next_calculation = input("Let's do next calculation? (yes/no): ")
-        if next_calculation == "no":
-          break
-    else:
-        print("Invalid Input, please enter within operation range")
